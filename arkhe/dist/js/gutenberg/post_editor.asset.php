5e2b156a556f52c4708f2d3289d4d9f7d14861b4
Sean Murphy <sean@iamseanmurphy.com>
2026-05-21 11:19:14 +0000

    3.13.0

--- arkhe/dist/js/gutenberg/post_editor.asset.php
+++ arkhe/dist/js/gutenberg/post_editor.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('wp-hooks'), 'version' => '3b0e12f36ca7247aebac');
+<?php return array('dependencies' => array('wp-hooks'), 'version' => 'fc26e29f15c9a5a173f7');
